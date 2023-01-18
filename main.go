7a9ac2806bb3c18ce766a931058a46408c693161
H1rono <hronok66@gmail.com>
2023-01-19 07:25:37 +0900

    set log level

--- main.go
+++ main.go
@@ -11,6 +11,27 @@ import (
 )
 
 func main() {
+	// TRACE, DEBUG, INFO, WARNING, ERROR, FATAL, PANIC
+	l := os.Getenv("LOG_LEVEL")
+	switch l {
+	case "TRACE":
+		log.SetLevel(log.TraceLevel)
+	case "DEBUG":
+		log.SetLevel(log.DebugLevel)
+	case "INFO", "":
+		log.SetLevel(log.InfoLevel)
+	case "WARNING":
+		log.SetLevel(log.WarnLevel)
+	case "ERROR":
+		log.SetLevel(log.ErrorLevel)
+	case "FATAL":
+		log.SetLevel(log.FatalLevel)
+	case "PANIC":
+		log.SetLevel(log.PanicLevel)
+	default:
+		log.Fatalf("Unexpected environment variable LOG_LEVEL=%s", l)
+	}
+
 	at := os.Getenv("BOT_ACCESS_TOKEN")
 	vt := os.Getenv("BOT_VERIFICATION_TOKEN")
 
