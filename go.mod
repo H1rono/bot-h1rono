b56739c4f22570a04e7c619f6e37b1d67db5870d
H1rono <hronok66@gmail.com>
2023-01-19 01:33:55 +0900

    add traPtitech/go-traq

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module git.trap.jp/H1rono_K/bot-h1rono
 
 go 1.19
 
-require github.com/traPtitech/traq-bot v1.0.2
+require (
+	github.com/traPtitech/go-traq v0.0.0-20220822084224-d405d26bce99
+	github.com/traPtitech/traq-bot v1.0.2
+)
+
+require (
+	github.com/golang/protobuf v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
+	traq "github.com/traPtitech/go-traq"
 	traqbot "github.com/traPtitech/traq-bot"
 )
 
 func main() {
+	at := os.Getenv("BOT_ACCESS_TOKEN")
 	vt := os.Getenv("BOT_VERIFICATION_TOKEN")
+
+	client := traq.NewAPIClient(traq.NewConfiguration())
+	auth := context.WithValue(context.Background(), traq.ContextAccessToken, at)
+
+	v, _, _ := client.ChannelApi.
+		GetChannels(auth).
+		IncludeDm(true).
+		Execute()
+	fmt.Printf("%#v", v)
 
 	handlers := traqbot.EventHandlers{}
 	handlers.SetJoinedHandler(func(payload *traqbot.JoinedPayload) {
