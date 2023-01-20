c6fc7b6e984b20011b69c6be5800bb16f3059ed6
H1rono <hronok66@gmail.com>
2023-01-20 11:31:06 +0900

    make package bot

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
-
 	traqbot "github.com/traPtitech/traq-bot"
+
+	"git.trap.jp/H1rono_K/bot-h1rono/bot"
 )
 
 func main() {
@@ -37,7 +38,7 @@ func main() {
 	uid := os.Getenv("BOT_USER_ID")
 	at := os.Getenv("BOT_ACCESS_TOKEN")
 	vt := os.Getenv("BOT_VERIFICATION_TOKEN")
-	b := NewBot(bid, uid, at, vt)
+	b := bot.NewBot(bid, uid, at, vt)
 
 	handlers := b.MakeHandlers()
 	server := traqbot.NewBotServer(vt, handlers)
